b818c71762006941667145a4434060b9eb75f6e7
caiogmrocha <caioguilherme739@gmail.com>
2022-08-20 21:18:05 -0300

    test: ensure Teacher returns with correct properties

---
+++ src/entities/teacher.spec.ts
@@ -0,0 +1,24 @@
+import { Teacher } from "./teacher"
+
+describe('Teacher Entity', () => {
+  it('should be able to create a Student instance', () => {
+    const teacher = new Teacher({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: 'password',
+      phone: '087999999999',
+      bio: 'fake bio here',
+      image: 'path/to/image',
+    })
+
+    expect(teacher.props).toEqual({
+      id: expect.any(String),
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: 'password',
+      phone: '087999999999',
+      bio: 'fake bio here',
+      image: 'path/to/image',
+    })
+  })
+})
