4f0bb454007972bcbcd19e97fd2c2fc4bd987466
caiogmrocha <caioguilherme739@gmail.com>
2022-08-27 15:53:23 -0300

    test: ensure RequiredFieldValidator returns RequiredFieldError if a empty string is provided

---
+++ src/validations/rules/required-field-validator.spec.ts
@@ -0,0 +1,31 @@
+import { RequiredFieldError } from "../errors/required-field-error"
+import { RequiredFieldValidator } from "./required-field-validator"
+
+describe('Required Field Validator', () => {
+  it('should return RequiredFieldError if a empty string is provided', () => {
+    const anyField = ''
+    const sut = new RequiredFieldValidator('any_field', anyField)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  it('should return RequiredFieldError if null is provided', () => {
+    const anyField = null
+    const sut = new RequiredFieldValidator('any_field', anyField)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  it('should return RequiredFieldError if undefined is provided', () => {
+    const anyField = undefined
+    const sut = new RequiredFieldValidator('any_field', anyField)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+})
