863c83154fa66cda67f7203a532720886e6321f0
caiogmrocha <caioguilherme739@gmail.com>
2022-08-27 15:55:56 -0300

    feat: ensure RegisterStudentController returns 422 if invalid data is provided

--- src/core/http/i-controller.ts
+++ src/core/http/i-controller.ts
@@ -1,5 +1,8 @@
+import { ValidationError } from '@/validations/errors/validation-error';
+import { Either } from '../logic/Either';
 import { HttpResponse } from './i-http-response';
 
 export interface IController<T = any> {
   handle(request: T): Promise<HttpResponse>;
+  validate?(request: T): Promise<Either<ValidationError, null>>;
 }
--- src/core/http/i-http-response.ts
+++ src/core/http/i-http-response.ts
@@ -1,3 +1,5 @@
+import { ValidationError } from "@/validations/errors/validation-error";
+
 export interface HttpResponse {
   body: any;
   statusCode: number;
@@ -7,5 +9,41 @@ export const created = (): HttpResponse => {
   return {
     body: null,
     statusCode: 201
+  };
+};
+
+export const clientError = (error: Error): HttpResponse => {
+  return {
+    body: {
+      error
+    },
+    statusCode: 400
+  };
+};
+
+export const conflict = (error: Error): HttpResponse => {
+  return {
+    body: {
+      error
+    },
+    statusCode: 403
+  }
+}
+
+export const unprocessable = (error: ValidationError): HttpResponse => {
+  return {
+    body: {
+      error
+    },
+    statusCode: 422
+  };
+};
+
+export const serverError = (message: string): HttpResponse => {
+  return {
+    body: {
+      message
+    },
+    statusCode: 500
   };
 };
