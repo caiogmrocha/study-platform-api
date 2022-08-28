9af130fe75f9f9ec49ed7aa1d486da063fde52be
caiogmrocha <caioguilherme739@gmail.com>
2022-08-28 12:37:40 -0300

    chore: create usecase generator

---
+++ .plop/usecase.js
@@ -0,0 +1,38 @@
+const { readdirSync } = require('fs')
+const path = require('path')
+
+const getDirectories = (source) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+module.exports = function (plop) {
+  plop.setGenerator('usecase', {
+    description: 'Base Use Case',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name',
+      },
+      {
+        type: 'list',
+        name: 'module',
+        message: 'Module',
+        choices: getDirectories(path.resolve(__dirname, '../src/modules'))
+      },
+    ],
+    actions: (answers) => {
+      const actions = [
+        {
+          type: 'add',
+          path: '../src/modules/{{module}}/{{dashCase name}}/{{dashCase name}}-use-case.ts',
+          templateFile: 'templates/usecase.ts.hbs',
+          skipIfExists: true,
+        }
+      ]
+
+      return actions
+    }
+  })
+}
