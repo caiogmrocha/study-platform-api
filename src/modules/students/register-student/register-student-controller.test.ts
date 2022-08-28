98fc9cb70fd9c81bb95e1777ea3047a2a9a161d3
caiogmrocha <caioguilherme739@gmail.com>
2022-08-27 23:06:59 -0300

    test: ensure RegisterStudentController returns 403 if the e-mail already exists

--- src/modules/students/register-student/register-student-controller.test.ts
+++ src/modules/students/register-student/register-student-controller.test.ts
@@ -1,5 +1,16 @@
 import { app } from '@/app'
+import { PrismaStudentsRepository } from '@/repositories/prisma-students-repository'
+import { ValidationError } from '@/validations/errors/validation-error'
 import request from 'supertest'
+import { StudentAlreadyExistsError } from './errors/student-already-exists-error'
+import { RegisterStudentUseCase } from './register-student-use-case'
+
+const makeRegisterStudentUseCase = (): RegisterStudentUseCase => {
+  const prismaStudentsRepository = new PrismaStudentsRepository()
+  const registerStudentUseCase = new RegisterStudentUseCase(prismaStudentsRepository)
+
+  return registerStudentUseCase
+}
 
 describe('[e2e] Register Student Controller', () => {
   it('should return 422 if invalid data is provided', async () => {
@@ -13,5 +24,32 @@ describe('[e2e] Register Student Controller', () => {
     })
 
     expect(response.status).toBe(422)
+    expect(response.body).toEqual(expect.objectContaining({
+      error: expect.objectContaining({
+        name: ValidationError.name
+      })
+    }))
+  })
+
+  it('should return 403 if the provided e-mail already exists', async () => {
+    const registerStudentUseCase = makeRegisterStudentUseCase()
+    const requestData = {
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password',
+      phone: '00000000000',
+      image: 'path/to/image',
+      bio: 'any_bio'
+    }
+
+    await registerStudentUseCase.execute(requestData)
+    const response = await request(app).post('/students/register').send(requestData)
+
+    expect(response.status).toBe(403)
+    expect(response.body).toEqual(expect.objectContaining({
+      error: expect.objectContaining({
+        name: StudentAlreadyExistsError.name
+      })
+    }))
   })
 })
