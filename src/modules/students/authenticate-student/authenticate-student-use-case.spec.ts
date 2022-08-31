c5038bdfe312223529224021e1500d90eb84d3dc
caiogmrocha <caioguilherme739@gmail.com>
2022-08-30 22:15:13 -0300

    test: ensure AuthenticateStudentUseCase returns StudentDoesNotExistsError if Student does not exists

---
+++ src/modules/students/authenticate-student/authenticate-student-use-case.spec.ts
@@ -0,0 +1,38 @@
+import { Student } from '@/entities/student';
+import { AuthenticateStudentUseCase } from '@/modules/students/authenticate-student/authenticate-student-use-case';
+import { InMemoryStudentsRepository } from '@/repositories/in-memory-students-repository';
+import { StudentDoesNotExistsError } from '../errors/student-does-not-exists-error';
+
+type SutTypes = {
+  sut: AuthenticateStudentUseCase
+}
+
+const makeSut = (students: Student[]): SutTypes => {
+  const studentRepository = new InMemoryStudentsRepository(students)
+  const sut = new AuthenticateStudentUseCase(studentRepository);
+
+  return { sut }
+}
+
+describe('Authenticate Student', () => {
+  it('should return StudentDoesNotExistsError if Student does not exists', async () => {
+    const { sut } = makeSut([
+      new Student({
+        name: 'any_name',
+        email: 'any@email.com',
+        password: 'any_password',
+        phone: '00000000000',
+        image: 'path/to/image',
+        bio: 'any_bio'
+      })
+    ])
+
+    const resultOrError = await sut.execute({
+      email: 'other@email.com',
+      password: 'other_password',
+    })
+
+    expect(resultOrError.isLeft()).toBeTruthy()
+    expect(resultOrError.value).toEqual(new StudentDoesNotExistsError('other@email.com', 'e-mail'))
+  })
+})
