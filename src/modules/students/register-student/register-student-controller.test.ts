f91ffb3dea45d064d021bb741d494840567df1e1
caiogmrocha <caioguilherme739@gmail.com>
2022-08-31 21:44:25 -0300

    fix: recreate RegisterStudentController

---
+++ src/modules/students/register-student/register-student-controller.test.ts
@@ -0,0 +1,93 @@
+import { app } from '@/app'
+import { PrismaStudentsRepository } from '@/repositories/prisma-students-repository'
+import { ValidationError } from '@/validations/errors/validation-error'
+import request from 'supertest'
+import { StudentAlreadyExistsError } from '../errors/student-already-exists-error'
+import { RegisterStudentUseCase } from './register-student-use-case'
+
+const makeRegisterStudentUseCase = (): RegisterStudentUseCase => {
+  const prismaStudentsRepository = new PrismaStudentsRepository()
+  const registerStudentUseCase = new RegisterStudentUseCase(prismaStudentsRepository)
+
+  return registerStudentUseCase
+}
+
+describe('[e2e] Register Student Controller', () => {
+  it('should return 201 if student has been registered', async () => {
+    const requestData = {
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password',
+      phone: '00000000000',
+      image: 'path/to/image',
+      bio: 'any_bio'
+    }
+
+    const response = await request(app).post('/students/register').send(requestData)
+
+    expect(response.status).toBe(201)
+    expect(response.body).toBeNull()
+  })
+
+  it('should return 422 if invalid data is provided', async () => {
+    const response = await request(app).post('/students/register').send({
+      name: '', // missing name param
+      email: 'any@email.com',
+      password: 'any_password',
+      phone: '00000000000',
+      image: 'path/to/image',
+      bio: 'any_bio'
+    })
+
+    expect(response.status).toBe(422)
+    expect(response.body).toEqual(expect.objectContaining({
+      error: expect.objectContaining({
+        name: ValidationError.name
+      })
+    }))
+  })
+
+  it('should return 403 if the provided e-mail already exists', async () => {
+    const registerStudentUseCase = makeRegisterStudentUseCase()
+    const requestData = {
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password',
+      phone: '00000000000',
+      image: 'path/to/image',
+      bio: 'any_bio'
+    }
+
+    await registerStudentUseCase.execute(requestData)
+    const response = await request(app).post('/students/register').send(requestData)
+
+    expect(response.status).toBe(403)
+    expect(response.body).toEqual(expect.objectContaining({
+      error: expect.objectContaining({
+        name: StudentAlreadyExistsError.name
+      })
+    }))
+  })
+
+  it('should return 403 if the provided phone already exists', async () => {
+    const registerStudentUseCase = makeRegisterStudentUseCase()
+    const requestData = {
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password',
+      phone: '00000000000',
+      image: 'path/to/image',
+      bio: 'any_bio'
+    }
+
+    await registerStudentUseCase.execute(requestData)
+    const response = await request(app).post('/students/register').send(requestData)
+
+    expect(response.status).toBe(403)
+    expect(response.body).toEqual(expect.objectContaining({
+      error: expect.objectContaining({
+        name: StudentAlreadyExistsError.name
+      })
+    }))
+  })
+})
