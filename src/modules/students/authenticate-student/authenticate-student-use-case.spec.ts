7dbe3edd3253be7b0cf851c3230ab5b51468a25e
caiogmrocha <caioguilherme739@gmail.com>
2022-09-01 20:34:54 -0300

    test: ensure AuthenticateStudentUseCase returns InvalidCredentialsError if the provided password is invalid

--- src/modules/students/authenticate-student/authenticate-student-use-case.spec.ts
+++ src/modules/students/authenticate-student/authenticate-student-use-case.spec.ts
@@ -1,6 +1,7 @@
 import { Student } from '@/entities/student';
 import { AuthenticateStudentUseCase } from '@/modules/students/authenticate-student/authenticate-student-use-case';
 import { InMemoryStudentsRepository } from '@/repositories/in-memory-students-repository';
+import { InvalidCrendentialsError } from '../errors/invalid-credentials-error';
 import { StudentDoesNotExistsError } from '../errors/student-does-not-exists-error';
 
 type SutTypes = {
@@ -34,5 +35,26 @@ describe('Authenticate Student', () => {
 
     expect(resultOrError.isLeft()).toBeTruthy()
     expect(resultOrError.value).toEqual(new StudentDoesNotExistsError('other@email.com', 'e-mail'))
+  })
+
+  it('should return InvalidCrendentialsError if the provided password is invalid', async () => {
+    const { sut } = makeSut([
+      new Student({
+        name: 'any_name',
+        email: 'any@email.com',
+        password: 'any_password',
+        phone: '00000000000',
+        image: 'path/to/image',
+        bio: 'any_bio'
+      })
+    ])
+
+    const resultOrError = await sut.execute({
+      email: 'any@email.com',
+      password: 'other_password',
+    })
+
+    expect(resultOrError.isLeft()).toBeTruthy()
+    expect(resultOrError.value).toEqual(new InvalidCrendentialsError())
   })
 })
