6a9b706e2590db4189b43b8414d60d64bfc7904c
caiogmrocha <caioguilherme739@gmail.com>
2022-09-01 21:05:58 -0300

    test: ensure AuthenticateStudentUseCase returns student data and JWT token if has been authenticated

--- src/modules/students/authenticate-student/authenticate-student-use-case.spec.ts
+++ src/modules/students/authenticate-student/authenticate-student-use-case.spec.ts
@@ -1,3 +1,4 @@
+import { adaptBcryptHash } from '@/core/adapters/bcrypt/bcrypt-hash-adapter';
 import { Student } from '@/entities/student';
 import { AuthenticateStudentUseCase } from '@/modules/students/authenticate-student/authenticate-student-use-case';
 import { InMemoryStudentsRepository } from '@/repositories/in-memory-students-repository';
@@ -56,5 +57,37 @@ describe('Authenticate Student', () => {
 
     expect(resultOrError.isLeft()).toBeTruthy()
     expect(resultOrError.value).toEqual(new InvalidCrendentialsError())
+  })
+
+  it('should return student data and JWT token if has been authenticated', async () => {
+    const { sut } = makeSut([
+      new Student({
+        name: 'any_name',
+        email: 'any@email.com',
+        password: await adaptBcryptHash('any_password'),
+        phone: '00000000000',
+        image: 'path/to/image',
+        bio: 'any_bio'
+      })
+    ])
+
+    const resultOrError = await sut.execute({
+      email: 'any@email.com',
+      password: 'any_password',
+    })
+
+    expect(resultOrError.isRight()).toBeTruthy()
+    expect(resultOrError.value).toEqual({
+      student: new Student({
+        id: expect.any(String),
+        name: 'any_name',
+        email: 'any@email.com',
+        password: expect.any(String),
+        phone: '00000000000',
+        image: 'path/to/image',
+        bio: 'any_bio'
+      }),
+      token: expect.any(String)
+    })
   })
 })
