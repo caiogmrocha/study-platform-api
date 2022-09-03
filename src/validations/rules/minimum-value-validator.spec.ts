0c527de92df9c1fd3668d828c0b5e84a1343c06c
caiogmrocha <caioguilherme739@gmail.com>
2022-09-02 23:52:00 -0300

    test: ensure MinimumValueValidator returns MinimumValueError if the provided string is invalid

---
+++ src/validations/rules/minimum-value-validator.spec.ts
@@ -0,0 +1,13 @@
+import { MinimumValueError } from "../errors/minimum-value-error"
+import { MinimumValueValidator } from "./minimum-value-validator"
+
+describe('Minimum Value Validator', () => {
+  it('should return MinimumValueError if the provided field is invalid string', () => {
+    const anyField = '1234567'
+    const sut = new MinimumValueValidator('any_field', anyField, 8)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new MinimumValueError('any_field', anyField, 8))
+  })
+})
