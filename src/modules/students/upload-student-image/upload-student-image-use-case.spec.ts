853bc3a2ddfbf69a86370c9a5b5abf4e29347656
caiogmrocha <caioguilherme739@gmail.com>
2022-09-09 23:13:14 -0300

    test: ensure UploadStudentImageUseCase save the image path of student

--- src/modules/students/upload-student-image/upload-student-image-use-case.spec.ts
+++ src/modules/students/upload-student-image/upload-student-image-use-case.spec.ts
@@ -2,6 +2,7 @@ import { Student } from '@/entities/student';
 import { UploadStudentImageUseCase } from '@/modules/students/upload-student-image/upload-student-image-use-case';
 import { IStudentsRepository } from '@/repositories/i-students-repository';
 import { InMemoryStudentsRepository } from '@/repositories/in-memory-students-repository';
+import { randomUUID } from 'crypto';
 import { StudentDoesNotExistsError } from '../errors/student-does-not-exists-error';
 
 type SutTypes = {
@@ -37,5 +38,35 @@ describe('Upload Student Image', () => {
 
     expect(resultOrError.isLeft()).toBeTruthy()
     expect(resultOrError.value).toEqual(new StudentDoesNotExistsError('any_id', 'id'))
+  })
+
+  it('should be able to save the image path of student', async () => {
+    const id = randomUUID()
+    const imagePath = 'path/to/image';
+    const { sut, studentRepository } = makeSut([
+      new Student({
+        id,
+        name: 'any_name',
+        email: 'any@email.com',
+        password: 'any_password',
+        phone: '00000000000',
+        image: null,
+        bio: 'any_bio'
+      })
+    ])
+
+    const resultOrError = await sut.execute({ id, imagePath })
+    const student = await studentRepository.findById(id)
+
+    expect(resultOrError.isRight()).toBeTruthy()
+    expect(student).toEqual(new Student({
+      id,
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password',
+      phone: '00000000000',
+      image: imagePath,
+      bio: 'any_bio'
+    }))
   })
 })
