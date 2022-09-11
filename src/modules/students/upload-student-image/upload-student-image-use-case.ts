0bbe0738c64543906965a6bfc9a3ce5a4398dbe4
caiogmrocha <caioguilherme739@gmail.com>
2022-09-11 15:25:55 -0300

    feat: ensure UploadStudentImageUseCase returns StudentDoesNotExistsError if student does not exists

--- src/modules/students/upload-student-image/upload-student-image-use-case.ts
+++ src/modules/students/upload-student-image/upload-student-image-use-case.ts
@@ -1,28 +1,31 @@
+import { File } from '@/core/file-system/file';
+import { IFileSystem } from '@/core/file-system/i-file-system';
 import { Either, left, right } from '@/core/logic/Either';
 import { IStudentsRepository } from '@/repositories/i-students-repository';
 import { StudentDoesNotExistsError } from '../errors/student-does-not-exists-error';
 
 export interface IUploadStudentImageDTO {
   id: string;
-  imagePath: string;
+  image: File;
 }
 
 export class UploadStudentImageUseCase {
   constructor (
-    private readonly studentRepository: IStudentsRepository
+    private readonly studentRepository: IStudentsRepository,
+    private readonly fileSystem: IFileSystem
   ) {}
 
-  async execute({ id, imagePath }: IUploadStudentImageDTO): Promise<Either<Error, null>> {
+  async execute({ id, image }: IUploadStudentImageDTO): Promise<Either<Error, null>> {
     const student = await this.studentRepository.findById(id)
 
     if (!student) {
       return left(new StudentDoesNotExistsError(id, 'id'))
     }
 
-    await this.studentRepository.update({
-      ...student.props,
-      image: imagePath,
-    }, id)
+    // await this.studentRepository.update({
+    //   ...student.props,
+    //   image: image.,
+    // }, id)
 
     return right(null)
   }
