0412905bbd7e48352efd9d3188ef375903edff9a
caiogmrocha <caioguilherme739@gmail.com>
2022-09-26 20:19:34 -0300

    test: ensure UploadStudentController returns 422 if the provided file not is in accepted mimetypes

--- src/modules/students/upload-student-image/upload-student-image-controller.test.ts
+++ src/modules/students/upload-student-image/upload-student-image-controller.test.ts
@@ -1,10 +1,37 @@
 import { app } from '@/app'
+import { BcryptEncryptionAdapter } from '@/core/encryption/bcrypt-encryption-adapter'
+import { PrismaStudentsRepository } from '@/repositories/prisma-students-repository'
+import { ValidationError } from '@/validations/errors/validation-error'
 import request from 'supertest'
+import { RegisterStudentUseCase } from '../register-student/register-student-use-case'
+
+let studentId: string;
 
 describe('[e2e] UploadStudentImageController', () => {
-  it('should return 200 if upload student image', async () => {
-    const response = await request(app).post('/students/id-upload-image')
+  beforeEach(async () => {
+    const prismaStudentsRepository = new PrismaStudentsRepository()
+    const encription = new BcryptEncryptionAdapter(10)
+    const registerStudentUseCase = new RegisterStudentUseCase(prismaStudentsRepository, encription)
 
-    expect(response.status).toBe(200)
+    const student = await registerStudentUseCase.execute({
+      name: 'any_name',
+      email: 'any@email.com',
+      password: 'any_password',
+      phone: '00000000000',
+      bio: 'any_bio'
+    })
+
+    studentId = (student.isRight() && student.value.id) as string
+  })
+
+  it('should return 422 if the provided file not is in accepted mimetypes', async () => {
+    const response = await request(app).post(`/students/upload-image/${studentId}`).attach('image', Buffer.from('any_thing'))
+
+    expect(response.status).toBe(422)
+    expect(response.body).toEqual(expect.objectContaining({
+      error: expect.objectContaining({
+        name: ValidationError.name
+      })
+    }))
   })
 })
