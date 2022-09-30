8a38af73d5fb1bb8357d54f6acb391fdfac4ba51
caiogmrocha <caioguilherme739@gmail.com>
2022-09-30 20:14:05 -0300

    test: ensure UpdateStudentUseCase returns StudentDoesNotExistsError if student does not exists

---
+++ src/modules/students/update-student/update-student-use-case.spec.ts
@@ -0,0 +1,46 @@
+import { Student } from '@/entities/student';
+import { UpdateStudentUseCase } from '@/modules/students/update-student/update-student-use-case';
+import { IStudentsRepository } from '@/repositories/i-students-repository';
+import { InMemoryStudentsRepository } from '@/repositories/in-memory-students-repository';
+import { randomUUID } from 'crypto';
+import { StudentDoesNotExistsError } from '../errors/student-does-not-exists-error';
+
+type SutTypes = {
+  sut: UpdateStudentUseCase;
+  studentsRepository: IStudentsRepository;
+}
+
+const makeSut = (students: Student[] = []): SutTypes => {
+  const studentsRepository = new InMemoryStudentsRepository(students)
+  const sut = new UpdateStudentUseCase(studentsRepository);
+
+  return { sut, studentsRepository }
+}
+
+describe('Update Student', () => {
+  it('should return StudentDoesNotExistsError if student does not exists', async () => {
+    const student = new Student({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: 'any_password',
+      phone: '00000000000',
+      bio: 'any_bio_here',
+      image: '',
+    })
+    const randomStudentId = randomUUID()
+    const { sut } = makeSut([ student ])
+
+    const resultOrError = await sut.execute({
+      authenticatedStudentId: student.id as string,
+      id: randomStudentId,
+      name: 'John Three',
+      email: 'john@three.com',
+      password: 'any_password',
+      phone: '11111111111',
+      bio: 'any_bio_here'
+    })
+
+    expect(resultOrError.isLeft()).toBeTruthy()
+    expect(resultOrError.value).toEqual(new StudentDoesNotExistsError(randomStudentId, 'id'))
+  })
+})
